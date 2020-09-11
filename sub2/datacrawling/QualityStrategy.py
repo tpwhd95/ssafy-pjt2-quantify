6f7685a9ce28c6720e09d5911c94de91232fb4e2
박세종 <tpwhd95@gmail.com>
2020-09-11 17:11:08 +0900

    S03P22A105-32 | FS
    Former-commit-id: 888a6cd3a5ba731725929078ed60c091811a85db

--- sub2/datacrawling/QualityStrategy.py
+++ sub2/datacrawling/QualityStrategy.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from FinancialStatement import FinancialStatement
+from FS import FS
 import requests
 from bs4 import BeautifulSoup
 from tabulate import tabulate
@@ -9,14 +9,14 @@ code_df.종목코드 = code_df.종목코드.map('{:06d}'.format)
 code_df = code_df[['회사명', '종목코드']]
 code_df = code_df.rename(columns={'회사명': 'name', '종목코드': 'code'})
 
-value_df = pd.DataFrame(columns=['종목', 'ROA', 'CFO', 'ROA_DIFF', 'ACCRUAL', 'EQ_OFFER', 'TURN', 'SUM'])
+value_df = pd.DataFrame(columns=['종목', 'ROA', 'CFO', 'ROA_DIFF', 'ACCRUAL', 'LIQUID', 'MARGIN', 'TURN', 'SUM'])
 
-for cnt in range(len(code_df)):
-# for cnt in range(250, 301):
+# for cnt in range(len(code_df)):
+for cnt in range(5):
     item_name = code_df.loc[cnt, 'name']
     code = code_df.loc[cnt, 'code']
     try:
-        fs = FinancialStatement(code)
+        fs = FS(code)
     except KeyError:
         continue
     cnt += 1
@@ -25,11 +25,12 @@ for cnt in range(len(code_df)):
     value_df.loc[cnt, ['CFO']] = 1 if fs.get_CFO() > 0 else 0
     value_df.loc[cnt, ['ROA_DIFF']] = 1 if fs.get_ROA_DIFF() > 0 else 0
     value_df.loc[cnt, ['ACCRUAL']] = 1 if fs.get_CFO() > fs.get_ROA() else 0
-    value_df.loc[cnt, ['EQ_OFFER']] = 1 if fs.get_EQ_OFFER() <= 0 else 0
-    value_df.loc[cnt, ['TURN']] = 1 if fs.get_TURN() > 0 else 0
+    value_df.loc[cnt, ['LIQUID']] = 1 if fs.get_LIQUID_cur() - fs.get_LIQUID_past() > 0 else 0
+    value_df.loc[cnt, ['MARGIN']] = 1 if fs.get_MARGIN_cur() - fs.get_MARGIN_past() > 0 else 0
+    value_df.loc[cnt, ['TURN']] = 1 if fs.get_TURN_cur() - fs.get_TURN_past() > 0 else 0
 
-value_df['SUM'] = value_df[['ROA', 'CFO', 'ROA_DIFF', 'ACCRUAL', 'EQ_OFFER', 'TURN']].sum(axis=1)
+value_df['SUM'] = value_df[['ROA', 'CFO', 'ROA_DIFF', 'ACCRUAL', 'LIQUID', 'TURN']].sum(axis=1)
 value_df = value_df.sort_values(by=["SUM"], ascending=[False])
 print(tabulate(value_df, headers='keys', tablefmt='psql'))
-print(len(value_df))
-value_df.to_csv('./Q.csv')
+# print(len(value_df))
+# value_df.to_csv('./Q.csv')
--- sub2/datacrawling/ValueStrategy.py
+++ sub2/datacrawling/ValueStrategy.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from FinancialStatement import FinancialStatement
+from FS import FS
 import requests
 from bs4 import BeautifulSoup
 from tabulate import tabulate
@@ -10,27 +10,24 @@ code_df.종목코드 = code_df.종목코드.map('{:06d}'.format)
 code_df = code_df[['회사명', '종목코드']]
 code_df = code_df.rename(columns={'회사명': 'name', '종목코드': 'code'})
 
-value_df = pd.DataFrame(columns=['종목', 'PER', 'PBR', 'PCR', 'PSR'])
-# value_df = pd.DataFrame(columns=['종목', 'PER', 'PBR'])
+value_df = pd.DataFrame(columns=['종목', 'PER', 'PBR', 'PSR'])
 
 for cnt in range(5):
     item_name = code_df.loc[cnt, 'name']
     code = code_df.loc[cnt, 'code']
-    fs = FinancialStatement(code)
+    fs = FS(code)
     cnt += 1
     value_df.loc[cnt, ['종목']] = item_name
     value_df.loc[cnt, ['PER']] = fs.get_PER()
     value_df.loc[cnt, ['PBR']] = fs.get_PBR()
-    value_df.loc[cnt, ['PCR']] = fs.get_PCR()
     value_df.loc[cnt, ['PSR']] = fs.get_PSR()
 
 value_df['PER'] = value_df['PER'].rank(axis=0)
 value_df['PBR'] = value_df['PBR'].rank(axis=0)
-value_df['PCR'] = value_df['PCR'].rank(axis=0)
 value_df['PSR'] = value_df['PSR'].rank(axis=0)
 # print(tabulate(value_df, headers='keys', tablefmt='psql'))
 
-value_df['RANK'] = value_df.apply(lambda row: (row['PER'] + row['PBR'] + row['PCR'] + row['PSR']),axis=1)
-# value_df['RANK'] = value_df[['PER', 'PBR', 'PCR', 'PSR']].sum(axis=1)
+value_df['RANK'] = value_df.apply(lambda row: (row['PER'] + row['PBR'] + row['PSR']),axis=1)
+# value_df['RANK'] = value_df[['PER', 'PBR', 'PSR']].sum(axis=1)
 value_df = value_df.sort_values(by=["RANK"], ascending=[True])
 print(tabulate(value_df, headers='keys', tablefmt='psql'))
