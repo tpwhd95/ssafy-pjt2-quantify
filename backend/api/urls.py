58d4545eb21b325b0cdbe95fb288edd91ee74e5e
tlagyqls <tlagyqls7@naver.com>
2020-09-21 09:27:36 +0900

    url hot fix
    Former-commit-id: 7851cbad7d75c1fdc160069e20b3bd36ab1fe916

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, include, re_path
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('auth/', include('users.urls')),
     path('strategy/', include('strategy.urls')),
     path('simulations/',include('simulations.urls')),
