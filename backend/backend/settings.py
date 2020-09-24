c265cb02bd3421dee8919d015d3291a8d6c24e3a
tlagyqls <tlagyqls7@naver.com>
2020-09-24 23:30:52 +0900

    투자 전략 뷰 수정
    Former-commit-id: e7129b2dc4aab7a7280347d48bdaefadeba837c9

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
 
     'users.apps.UsersConfig',
     'simulations',
-    'api',
+    
 ]
 
 MIDDLEWARE = [
--- backend/strategy/urls.py
+++ backend/strategy/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views
-
+from django.urls import path,re_path
+from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
+from . import views
 urlpatterns = [
-    path('getlowvar/', views.lowvarlist),
-    path('getmomen/', views.momenlist),
-    path('getriskmomen/', views.riskmomenlist),
-    path('getvalue/', views.valuelist),
-    path('getquality/', views.qualitylist),
+    path('lowvar', views.LowVariabilityView.as_view()),
+    path('momen', views.MomentumView.as_view()),
+    path('riskmomen', views.RiskMomentumView.as_view()),
+    path('value', views.ValueView.as_view()),
+    path('quality', views.QualityView.as_view()),
 ]
