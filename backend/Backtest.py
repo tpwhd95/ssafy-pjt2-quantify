ce594ccd6f4739a753d8fd6f4324ed808028b3c1
tlagyqls <tlagyqls7@naver.com>
2020-10-03 18:06:01 +0900

    S03P23A105-35 | 백테스트 클래스 완성
    Former-commit-id: 5f6097aa3834a65fc7da3a252e3816cf2bbe875e

--- backend/Backtest.py
+++ backend/Backtest.py
@@ -16,10 +16,12 @@ class Backtest:
         self.current = start
         self.strategy = strategy
         self.budget = budget
+        self.start_budget = budget
         self.rebalance = rebalance
         self.df = pd.DataFrame(columns=['date','budget'])
-        self.stock_list = [{'code':'155660'}]
+        self.stock_list = []
         self.lv = LV()
+        self.divide = 5
     def run(self):
         day = datetime.timedelta(days=1)
         delta = datetime.timedelta(days=self.rebalance*30)
@@ -35,10 +37,17 @@ class Backtest:
             # df_marks = df_marks.append(new_row, ignore_index=True)
             # day_date = date
             end = date + delta if date + delta <= self.end else self.end
-            
+            print(self.stock_list)
             while date < end:
-                self.df = self.df.append({'date':date,'budget':self.get_budget(date)},ignore_index=True)
-                # self.df.loc[cnt]={'date':day_date,'budget':self.get_budget(day_date)}
+                cur_budget = self.get_budget(date)
+                if cur_budget == self.budget:
+                    if date == self.start:
+                        cur_budget = self.start_budget
+                    else:
+                        cur_budget = self.df[self.df['date']==(date-datetime.timedelta(days=1)).strftime("%Y-%m-%d")]['budget'].values[0]
+                cur_budget = int(cur_budget)
+                self.df = self.df.append({'date':date,'budget':cur_budget},ignore_index=True)
+                print(date)
                 date+=day
                 cnt+=1
             
@@ -46,7 +55,7 @@ class Backtest:
                 #리밸런싱 기간동안 돌면서 데이타프래임에 현재 예산 넣고
             #다 판매
             
-            self.sell_stock()
+            self.sell_stock(date)
 
             if date < self.end:
 
@@ -57,11 +66,50 @@ class Backtest:
         return self.df
         
     def buy_stock(self,date):
+
         table = self.lv.run(date)
-        print(table)
-        # pass
-    def sell_stock(self):
-        pass
+        budget_divide = self.budget/self.divide
+        for i in range(self.divide):
+            code = table.iloc[i].종목
+
+            stock = StockPrice.objects.get(code=code)
+            stock = model_to_dict(stock)
+            df = pd.DataFrame(stock['data'])
+            cur_date = date
+            price = 0
+            while price==0:
+                temp = df.loc[df['time'] == cur_date.strftime('%Y-%m-%d')]['close'].mean()
+                if np.isnan(temp):
+                    price=0
+                else:
+                    price=temp
+                print(price)
+
+                cur_date -= datetime.timedelta(days=1)
+            print(price)
+            quantity = budget_divide//price
+            self.stock_list.append({"code":code,"quantity":quantity,"price":price})
+            self.budget -= price*quantity
+
+    def sell_stock(self,date):
+        print("sell_start")
+        for i in self.stock_list:
+            stock = StockPrice.objects.get(code=i['code'])
+            stock = model_to_dict(stock)
+            df = pd.DataFrame(stock['data'])
+            cur_date = date
+            price = 0
+            while price ==0 :
+                temp = df.loc[df['time'] == cur_date.strftime('%Y-%m-%d')]['close'].mean()
+                if np.isnan(temp):
+                    price=0
+                else:
+                    price=temp
+                print(price)
+                cur_date -= datetime.timedelta(days=1)
+            self.budget += i['quantity'] * price
+        self.stock_list=[]
+
     def get_budget(self,day):
         stock_sum = 0
         for row in self.stock_list:
@@ -71,7 +119,7 @@ class Backtest:
             r = df.loc[df['time'] == day.strftime('%Y-%m-%d')]
             if len(r)==0:
                 continue
-            stock_sum += r['close'].values[0]
+            stock_sum += r['close'].values[0]*row['quantity']
         return self.budget+stock_sum
-a = Backtest("2020-01-20","2020-06-20",1,200000,3)
+a = Backtest("2017-01-20","2018-01-20",1,2000000,3)
 print(a.run())
