02e5c0e75f9f530c13bf61c2d8f8bee1e121aca4
tlagyqls <tlagyqls7@naver.com>
2020-10-07 13:45:23 +0900

    캐시 추가
    Former-commit-id: f3f411767873b261525565136050e5f2045723a9

--- backend/strategy/views.py
+++ backend/strategy/views.py
@@ -7,38 +7,38 @@ from rest_framework.permissions import AllowAny
 
 import json
 from .serializers import MomentumSerializer, LowVariabilitySerializer, RiskMomentumSerializer, ValueSerializer, QualitySerializer
-
+from django.core.cache import cache
 
 @permission_classes((AllowAny,))
 class MomentumView(APIView):
     def get(self,request):
-        momentum = Momentum.objects.all()
+        momentum = cache.get_or_set("momentum",Momentum.objects.all())
         return Response(MomentumSerializer(momentum,many=True).data,status=status.HTTP_200_OK)
 
 
 @permission_classes((AllowAny,))
 class QualityView(APIView):
     def get(self,request):
-        quality = Quality.objects.all()
+        quality = cache.get_or_set("quality",Quality.objects.all())
         return Response(QualitySerializer(quality,many=True).data,status=status.HTTP_200_OK)
 
 
 @permission_classes((AllowAny,))
 class RiskMomentumView(APIView):
     def get(self,request):
-        riskMomentum = RiskMomentum.objects.all()
+        riskMomentum = cache.get_or_set("risk_momentum",RiskMomentum.objects.all())
         return Response(RiskMomentumSerializer(riskMomentum,many=True).data,status=status.HTTP_200_OK)
 
 
 @permission_classes((AllowAny,))
 class ValueView(APIView):
     def get(self,request):
-        value = Value.objects.all()
+        value = cache.get_or_set("value",Value.objects.all())
         return Response(ValueSerializer(value,many=True).data,status=status.HTTP_200_OK)
 
 
 @permission_classes((AllowAny,))
 class LowVariabilityView(APIView):
     def get(self,request):
-        lowVariability = LowVariability.objects.all()
+        lowVariability = cache.get_or_set("low_variability",LowVariability.objects.all())
         return Response(LowVariabilitySerializer(lowVariability,many=True).data,status=status.HTTP_200_OK)
